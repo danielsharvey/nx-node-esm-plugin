544838ef9f8cad34546e8b08e6e3a4d37ee7f4d6
Daniel Harvey <daniel@harves.net>
2024-06-02 15:05:28 +0800

    chore: Move test apps into apps/ folder

--- apps/testapp1-commonjs/src/index.ts
+++ apps/testapp1-commonjs/src/index.ts
@@ -1,4 +1,4 @@
-import { testapp1Commonjs } from "./lib/testapp1-commonjs";
+import { testapp1Commonjs } from './lib/testapp1-commonjs';
 
 (async function() {
   console.log('TEST', await testapp1Commonjs());
