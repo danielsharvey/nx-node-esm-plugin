c59c94b9626e41d126a1aa74db51ccd2f7f2e491
Daniel Harvey <daniel@harves.net>
2024-06-03 09:42:22 +0800

    test: Implement node executor test

--- libs/nx-node-esm-plugin/src/executors/node/executor.spec.ts
+++ libs/nx-node-esm-plugin/src/executors/node/executor.spec.ts
@@ -1,11 +1,40 @@
 import { NodeExecutorSchema } from './schema';
 import executor from './executor';
+import { ExecutorContext } from '@nx/devkit';
+import { resolveFileToRunFromPackageJson } from './package-json-utils';
 
-const options: NodeExecutorSchema = {};
+const options: NodeExecutorSchema = {
+  buildTarget: 'build',
+  _: [],
+  __unparsed__: [],
+};
 
-describe('Node Executor', () => {
-  it('can run', async () => {
-    const output = await executor(options);
-    expect(output.success).toBe(true);
+describe('Node executor', () => {
+  it('can resolve fileToRun from a package.json', async () => {
+
+    const fileToRun = resolveFileToRunFromPackageJson(
+      'libs/nx-node-esm-plugin/src/executors/node/__fixtures__'
+    );
+    expect(fileToRun).toBe(
+      'libs/nx-node-esm-plugin/src/executors/node/__fixtures__/index.js'
+    );
+
   });
+
+  // performed as E2E test
+  // it('can run', async () => {
+
+  //   const context: ExecutorContext = {
+  //     root: '/root',
+  //     projectName: 'test',
+  //     targetName: 'test',
+  //     configurationName: 'development',
+  //     cwd: '/tmp',
+  //     isVerbose: false,
+  //   };
+
+  //   const output = await executor(options, context);
+  //   expect(output.success).toBe(true);
+
+  // });
 });
