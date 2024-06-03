4138e1df9baea5666a9f3a3a50a1a83cef28f3fb
Daniel Harvey <daniel@harves.net>
2024-06-03 11:19:29 +0800

    test: Implement node-runner library test

--- libs/node-runner/src/lib/node-runner.spec.ts
+++ libs/node-runner/src/lib/node-runner.spec.ts
@@ -1,7 +1,33 @@
-import { nodeRunner } from './node-runner';
+import { workspaceRoot } from '@nx/devkit';
+import { fork } from 'child_process';
+import { join } from 'path';
 
 describe('nodeRunner', () => {
-  it('should work', () => {
-    expect(nodeRunner()).toEqual('node-runner');
+  it('should run Node script', async () => {
+    expect.assertions(1);
+
+    const result = await new Promise((res, rej) => {
+      let stdout = '';
+      const child = fork(
+        join(workspaceRoot, 'dist/libs/node-runner/src/lib/node-runner.js'),
+        [],
+        {
+          env: {
+            NX_FILE_TO_RUN: join(
+              workspaceRoot,
+              'libs/node-runner/src/lib/__fixtures__/node-runner-test-script.js'
+            ),
+          },
+          stdio: ['pipe', 'pipe', 'inherit', 'ipc'],
+        }
+      );
+      child.stdout?.on('data', (data) => (stdout += data.toString()));
+      child.on('error', (err) => rej(err));
+      child.on('exit', (code) => {
+        if (code === 0) res(stdout);
+        else rej(new Error(`fork() failed with exit code ${code}`));
+      });
+    });
+    expect(result).toMatch(/^node-runner-test-script/);
   });
 });
